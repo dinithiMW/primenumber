bf941c21f8997e0da102ed583f44b561ea9c40e2
[dinithi] <[dinithimweerasinghe95@gmail.com>
2019-02-10 23:23:52 +0530

    primenumber
    primenumber

---
+++ Prime.java
@@ -0,0 +1,23 @@
+
+public class Prime {
+    public static void main(String args[]){
+    
+        for(int i=0;i<=10;i++){
+        int count=0;
+        for(int j=0;j<=i;j++);
+            int j;
+        int ans= i % j;
+        if (ans==0){
+            count++;
+        }
+        
+        
+        
+        }
+        int count;
+        if(count==2){
+            System.out.println(i);
+        }
+    }
+    
+}
